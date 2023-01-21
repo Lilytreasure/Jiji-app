067f5cfa382911551cb8c3fb51f11550ead2c857
Lilytreasure <lilyngure@gmail.com>
2023-01-21 15:13:56 +0300

    set up

--- app/src/main/java/com/example/jijiapp/MainActivity.kt
+++ app/src/main/java/com/example/jijiapp/MainActivity.kt
@@ -2,10 +2,70 @@ package com.example.jijiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.jijiapp.Fragments.FragmentHome
+import com.example.jijiapp.Fragments.FragmentSaved
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    //A treplica of the JIJI mobile application
+    private lateinit var  bottomNavigationView: BottomNavigationView
+
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView=findViewById(R.id.BottomView)
+
+
+        //fragmentHome is populated by default
+
+        replaceFragments(FragmentHome())
+
+
+
+        bottomNavigationView.setOnItemSelectedListener(){
+            when(it.itemId){
+                R.id.hmBtn->replaceFragments(FragmentHome())
+                R.id.svBtn->replaceFragments(FragmentSaved())
+            }
+
+
+            true
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //onCreate method ends here
     }
+
+
+    //a function to replace the fragments
+    private fun replaceFragments(fragment: Fragment){
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainView,fragment)
+        fragmentTransaction.commit()
+
+
+    }
+
+
+
 }
